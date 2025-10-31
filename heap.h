93255bfbd350b282450be50a96758e3e1914415c
Chris <cpalomares2522@sdsu.edu>
2025-10-30 23:13:10 -0700

    Implemented MinHeap pop() and downheap() and tested current methods to see if they work

--- heap.h
+++ heap.h
@@ -25,10 +25,15 @@ struct MinHeap {
     }
 
     int pop(int weightArr[]) {
-        // TODO: remove and return smallest index
         // Replace root with last element, then call downheap()
+        if (size == 0)
+            return -1;
 
-        return -1; // placeholder
+        int rootElement = data[0];      // store root element
+        data[0] = data[size - 1];       // Move last element to the root
+        size--;                         // Reduce heap size
+        downheap(0, weightArr);     // Restore heap order
+        return rootElement;             // Return smallest index
     }
 
     void upheap(int pos, int weightArr[]) {
@@ -49,7 +54,35 @@ struct MinHeap {
     }
 
     void downheap(int pos, int weightArr[]) {
-        // TODO: swap parent downward while larger than any child
+        // swap parent downward while larger than any child
+        while (true) {
+            int left = 2 * pos + 1;
+            int right = 2 * pos + 2;
+            int smallest = pos;
+
+            // compare parent with left child
+            if (left < size && weightArr[data[left]] < weightArr[data[smallest]]) {
+                smallest = left;
+            }
+
+            // compare parent with right child
+            if (right < size && weightArr[data[right]] < weightArr[data[smallest]]) {
+                smallest = right;
+            }
+
+            // stop if parent is already smallest
+            if (smallest == pos) {
+                break;
+            }
+
+            // Swap parent and smaller child
+            int temp = data[pos];
+            data[pos] = data[smallest];
+            data[smallest] = temp;
+
+            // Continue down the tree
+            pos = smallest;
+        }
     }
 };
 
